f0ffedd422f06a2043709efe1121e7ad20e5887f
chen zhi <3231183282@qq.com>
2024-04-01 23:53:08 +0800

    Site updated: 2024-04-01 23:53:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/31/使用hexo写blog/","2024/03/31/hello-world/"];function toRandomPost(){
+var posts=["2024/03/31/hello-world/","2024/03/31/使用hexo写blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
