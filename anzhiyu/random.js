6ab683572a44bf639d7b70872c1637d9806ce118
chen zhi <3231183282@qq.com>
2024-04-17 11:49:45 +0800

    Site updated: 2024-04-17 11:49:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/04/learnJava/","2024/04/03/template/","2024/04/15/hello-world/","2024/03/31/使用hexo写blogmarkdown简易教学/"];function toRandomPost(){
+var posts=["2024/04/15/hello-world/","2024/03/31/使用hexo写blogmarkdown简易教学/","2024/04/04/learnJava/","2024/04/03/template/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
